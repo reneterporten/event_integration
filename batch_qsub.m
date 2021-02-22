42e50852f36754395d89c2c7336e77922673a92d
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2021-02-22 16:37:44 +0100

    ENH - some changes

--- batch_qsub.m
+++ batch_qsub.m
@@ -9,12 +9,29 @@ subjlist = {'sub-004' 'sub-005' 'sub-006' 'sub-008' 'sub-009' 'sub-010' ...
  'sub-035' 'sub-037'};
 
 
+% cfgfreq = [];
+% cfgfreq.method = 'mtmconvol';
+% cfgfreq.foi    = 12.5:2.5:40;
+% cfgfreq.t_ftimwin = ones(1,numel(cfgfreq.foi)).*0.4;
+% cfgfreq.taper  = 'dpss';
+% cfgfreq.tapsmofrq = ones(1,numel(cfgfreq.foi)).*5;
+% cfgfreq.toi    = (-150:15:450)./300;
+% cfgfreq.pad    = 4;
+% 
+% for k = 1:numel(subjlist)
+%   % old syntax
+%   %qsubfeval('rt_sensorlevelanalysis', subjlist{k}, [], 'tfr', 1, savedir, 'memreq', 12*1024^3, 'timreq', 60*60, 'batchid', subjlist{k});
+% 
+%   qsubfeval('rt_sensorlevelanalysis', subjlist{k}, 'type', 'tfr', 'saveflag', savedir, 'savename', 'tfr_mid', 'cfgfreq', cfgfreq, 'memreq', 12*1024^3, 'timreq', 60*60, 'batchid', subjlist{k});
+% 
+% end
+
 cfgfreq = [];
 cfgfreq.method = 'mtmconvol';
-cfgfreq.foi    = 12.5:2.5:40;
+cfgfreq.foi    = 30:2.5:90;
 cfgfreq.t_ftimwin = ones(1,numel(cfgfreq.foi)).*0.4;
 cfgfreq.taper  = 'dpss';
-cfgfreq.tapsmofrq = ones(1,numel(cfgfreq.foi)).*5;
+cfgfreq.tapsmofrq = ones(1,numel(cfgfreq.foi)).*10;
 cfgfreq.toi    = (-150:15:450)./300;
 cfgfreq.pad    = 4;
 
@@ -22,6 +39,6 @@ for k = 1:numel(subjlist)
   % old syntax
   %qsubfeval('rt_sensorlevelanalysis', subjlist{k}, [], 'tfr', 1, savedir, 'memreq', 12*1024^3, 'timreq', 60*60, 'batchid', subjlist{k});
 
-  qsubfeval('rt_sensorlevelanalysis', subjlist{k}, 'type', 'tfr', 'saveflag', savedir, 'savename', 'tfr_mid', 'cfgfreq', cfgfreq, 'memreq', 12*1024^3, 'timreq', 60*60, 'batchid', subjlist{k});
+  qsubfeval('rt_sensorlevelanalysis', subjlist{k}, 'type', 'tfr', 'saveflag', savedir, 'savename', 'tfr_high', 'cfgfreq', cfgfreq, 'memreq', 12*1024^3, 'timreq', 60*60, 'batchid', subjlist{k});
 
 end
